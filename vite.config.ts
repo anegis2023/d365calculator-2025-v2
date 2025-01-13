fd3ab0f36933e2a8860b540a5e8630f57cca4273
anegis2023 <pjaworski@anegis.com>
2025-01-13 14:36:43 +0100

    chore: update favicon links to use CDN URLs

--- vite.config.ts
+++ vite.config.ts
@@ -14,11 +14,11 @@ function generateMetaTags(route: string, mode: string) {
 
   // 2. Icon
   const iconTag = `
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-    <link rel="manifest" href="/site.webmanifest" />`;
+    <link rel="icon" type="image/png" sizes="32x32" href="https://cdn.prod.website-files.com/5c8f846a0fcf5a79e8c7dd24/67851581b0aead5e01fdca3d_favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="https://cdn.prod.website-files.com/5c8f846a0fcf5a79e8c7dd24/67851581c14e0e15b22ff799_favicon-16x16.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="https://cdn.prod.website-files.com/5c8f846a0fcf5a79e8c7dd24/67851581105df1f5c364dbcf_apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="192x192" href="https://cdn.prod.website-files.com/5c8f846a0fcf5a79e8c7dd24/678515814b0a3bb9bec17281_android-chrome-192x192.png" />
+    <link rel="icon" type="image/png" sizes="512x512" href="https://cdn.prod.website-files.com/5c8f846a0fcf5a79e8c7dd24/6785158139494742637d746f_android-chrome-512x512.png" />`;
 
   // 3. Title and basic meta tags
   const basicTags = `
